55e56f5f6e9b76fbbba69d50652dfc88636957cc
Sam Loughlin <samloughlin@gmail.com>
2020-03-14 17:08:52 +0000

    Added scanning with test

--- src/checkout/Checkout.cs
+++ src/checkout/Checkout.cs
@@ -1,7 +1,10 @@
-﻿namespace checkout
+﻿using System.Collections.Generic;
+
+namespace checkout
 {
     public class Checkout
     {
+        public IList<Item> ScannedItems { get; } = new List<Item>();
         public decimal Total()
         {
             return 0m;
@@ -9,11 +12,17 @@
 
         public void Scan(Item item)
         {
-
+            ScannedItems.Add(item);
         }
     }
 
-    public class Item{
+    public class Item
+    {
+        public Item(string sku, decimal unitPrice)
+        {
+            Sku = sku;
+            UnitPrice = unitPrice;
+        }
         public string Sku { get; set; }
         public decimal UnitPrice { get; set; }
     }
--- test/checkout.tests/CheckoutTests.cs
+++ test/checkout.tests/CheckoutTests.cs
@@ -7,8 +7,15 @@ namespace checkout.tests
     public class CheckoutTests
     {
         [Test]
-        public void Test(){
-            Assert.Fail();
+        public void WhenScanCalled_ScannedItemContainsTheItem(){
+            var target = new Checkout();
+
+            var item = new Item("A99", 0.50m);
+
+
+            Assert.That(target.ScannedItems, Does.Not.Contain(item));
+            target.Scan(item);
+            Assert.That(target.ScannedItems, Does.Contain(item));
         }
     }
 }
