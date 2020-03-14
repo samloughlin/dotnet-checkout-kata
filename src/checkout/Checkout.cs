f48cb9b0df55ca3e6a497acf7c3438b78b5587ca
Sam Loughlin <samloughlin@gmail.com>
2020-03-14 17:50:18 +0000

    Simple implementation of offers. Doesn't handle multiple of same offers

--- src/checkout/Checkout.cs
+++ src/checkout/Checkout.cs
@@ -5,16 +5,52 @@ namespace checkout
 {
     public class Checkout
     {
+        public IList<SpecialOffer> SpecialOffers { get; } = new List<SpecialOffer>{
+            new SpecialOffer("A99", 3, 1.30m),
+            new SpecialOffer("B15", 2, 0.45m)
+        };
+
         public IList<Item> ScannedItems { get; } = new List<Item>();
         public decimal Total()
         {
-            return ScannedItems.Sum(s => s.UnitPrice);
+            var itemsToProcess = ScannedItems.ToList();
+            var total = 0m;
+            foreach (var offer in SpecialOffers)
+            {
+                if (itemsToProcess.Count(s => s.Sku == offer.Sku) >= offer.Quantity)
+                {
+                    total += offer.OfferPrice;
+                    var countToRemove = offer.Quantity;
+
+                    var i = itemsToProcess.Count() - 1;
+                    while (countToRemove != 0)
+                    {
+                        var currentItem = itemsToProcess[i];
+                        if (currentItem.Sku == offer.Sku && countToRemove != 0)
+                        {
+                            itemsToProcess.RemoveAt(i);
+                            countToRemove--;
+                        }
+                        else if (countToRemove == 0)
+                        {
+                            break;
+                        }
+                        i--;
+                    }
+                }
+
+            }
+
+            total += itemsToProcess.Sum(s => s.UnitPrice);
+            return total;
         }
 
         public void Scan(Item item)
         {
             ScannedItems.Add(item);
         }
+
+
     }
 
     public class Item
@@ -26,5 +62,19 @@ namespace checkout
         }
         public string Sku { get; set; }
         public decimal UnitPrice { get; set; }
+    }
+
+    public class SpecialOffer
+    {
+        public SpecialOffer(string sku, int quantity, decimal offerPrice)
+        {
+            Sku = sku;
+            Quantity = quantity;
+            OfferPrice = offerPrice;
+        }
+
+        public string Sku { get; }
+        public int Quantity { get; }
+        public decimal OfferPrice { get; }
     }
 }
